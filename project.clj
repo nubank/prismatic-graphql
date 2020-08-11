85b399851950f752717a813156998d2af3b68c6b
Leonardo Iacovini <leoiacovini@gmail.com>
2020-08-10 23:59:06 -0300

    Add Apache 2.0 LICENSE

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject prismatic-graphql "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Clojure library for creating Prismatic Schemas from GraphQL Queries"
+  :url "https://github.com/nubank/prismatic-graphql"
+  :license {:name "Apache 2.0"
+            :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [alumbra/parser "0.1.6"]
                  [prismatic/schema "1.1.11"]
