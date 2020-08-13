e9538158ef2b8d8c9ef65e2c3fbb2e65f702cf17
Leonardo Iacovini <leoiacovini@gmail.com>
2020-08-13 14:04:54 -0300

    Bump alumbra.parser to support description on graphql schema

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache 2.0"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [leoiacovini/alumbra.parser "0.1.8-SNAPSHOT"]
+                 [leoiacovini/alumbra.parser "0.1.9-SNAPSHOT"]
                  [prismatic/schema "1.1.11"]
                  [alumbra/validator "0.2.1"]
                  [alumbra/analyzer "0.1.17"]]
